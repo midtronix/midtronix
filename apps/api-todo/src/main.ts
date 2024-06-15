8ebef6feb9ef62bae5d0c2ac58998f6f5ad1b095
midtronix <ahmet@midtronix.com>
2024-06-15 14:14:57 -0500

    api intial configuration

--- apps/api-todo/src/main.ts
+++ apps/api-todo/src/main.ts
@@ -7,15 +7,40 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+
+  const cfg = app.get(ConfigService);
+
+  const appName = cfg.getOrThrow('appName');
+  const appVersion = cfg.getOrThrow('appVersion');
+  const appDescription = cfg.getOrThrow('appDescription');
+  const globalPrefix = cfg.getOrThrow('globalPrefix');
+  const host = cfg.getOrThrow('host');
+  const port = cfg.getOrThrow('port');
+  const swaggerPath = cfg.getOrThrow('swaggerPath');
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.enableCors({ origin: '*' });
+
+  const docConfig = new DocumentBuilder()
+    .setTitle(appName)
+    .setDescription(appDescription)
+    .setVersion(appVersion)
+    .addTag(appName)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+
+  SwaggerModule.setup(swaggerPath, app, document);
+
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://${host}:${port}/${globalPrefix}`
   );
 }
 
