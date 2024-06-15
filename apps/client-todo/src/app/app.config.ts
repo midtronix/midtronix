3857c89cf515e2ac85d74a954c31341d195fd915
midtronix <ahmet@midtronix.com>
2024-06-15 13:34:18 -0500

    initial nest app

--- apps/client-todo/src/app/app.config.ts
+++ apps/client-todo/src/app/app.config.ts
@@ -8,16 +8,24 @@ import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideStore } from '@ngrx/store';
+import { provideEntityData, withEffects } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { provideEffects } from '@ngrx/effects';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
+
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideHttpClient(),
     provideStore(),
+    provideEntityData(entityConfig, withEffects()),
+    provideEffects([]),
   ],
 };
